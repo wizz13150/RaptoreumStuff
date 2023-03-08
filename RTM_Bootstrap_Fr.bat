c53a3a8f1c0226a09bc94dec0dfda438fd719af8
Wizz_^ <lewizard@hotmail.fr>
2023-03-08 08:34:37 +0100

    Update RTM_Bootstrap_Fr.bat

--- RTM_Bootstrap_Fr.bat
+++ RTM_Bootstrap_Fr.bat
@@ -4,12 +4,12 @@ set DOWNLOAD_URL=https://raw.githubusercontent.com/wizz13150/RaptoreumStuff/main
 set DOWNLOAD_PATH=%CD%\RTM_Bootstrap_Fr.ps1
 
 echo Telechargement du fichier depuis %DOWNLOAD_URL% ...
-powershell.exe -Command "& { Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile '%DOWNLOAD_PATH%' }"
+powershell.exe -Command "& { Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -UseBasicParsing -OutFile '%DOWNLOAD_PATH%' }"
 
 if exist "%DOWNLOAD_PATH%" (
-echo Fichier telecharge avec succes !
-echo Execution de RTM_Bootstrap_Fr.ps1...
-powershell.exe -ExecutionPolicy RemoteSigned -File "%DOWNLOAD_PATH%"
+    echo Fichier telecharge avec succes !
+    echo Execution de RTM_Bootstrap_Fr.ps1...
+    powershell.exe -ExecutionPolicy RemoteSigned -Command "Set-Content -Path '%DOWNLOAD_PATH%' -Value (Get-Content -Path '%DOWNLOAD_PATH%' -Encoding utf8) -Encoding utf8; & '%DOWNLOAD_PATH%'"
 ) else (
-echo Echec du telechargement du fichier.
+    echo Echec du telechargement du fichier.
 )
