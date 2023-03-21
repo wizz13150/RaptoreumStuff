7158825dc61622fb8d558bff658903b4c3b300ab
Wizz_^ <lewizard@hotmail.fr>
2023-03-21 22:45:07 +0100

    Update RTM_Bootstrap.ps1

--- RTM_Bootstrap.ps1
+++ RTM_Bootstrap.ps1
@@ -12,7 +12,7 @@ $walletDirectory = "$env:APPDATA\raptoreumcore"
 $bootstrapZipPath = "$env:APPDATA\raptoreumcore\bootstrap.zip"
 
 # Other fixed variables
-$walletProcessName = "raptoreum*"
+$walletProcessName = "raptoreum-qt.exe"
 $dateSuffix = Get-Date -Format "ddMMyyyy"
 $bootstrapUrl = "https://bootstrap.raptoreum.com/bootstraps/bootstrap.zip"
 $blocksDirectory = "$walletDirectory\blocks"
